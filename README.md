edad8ea4709dd92d62ca08a05a1c344d7f0dc227
Nick Romanov <romanov.aero@gmail.com>
2018-11-08 20:31:44 +0300

    fixes

--- README.md
+++ README.md
@@ -8,13 +8,10 @@ In order to start the service, the following dependencies are required:
 
 * **Go:** service is written in Go. You need to install it on your system
 * **dep:** this project uses dep to manage dependencies as well as to provide reproducible builds
-* **reindexer:** this project uses reindexer inmemory db
+* **docker:** this project uses doсker
 
 ```
 go get -u github.com/golang/dep/cmd/dep
-go get -a github.com/restream/reindexer
-bash $GOPATH/src/github.com/restream/reindexer/dependencies.sh
-go generate github.com/restream/reindexer/bindings/builtin
 ```
 
 ### Installing
@@ -23,16 +20,18 @@ go generate github.com/restream/reindexer/bindings/builtin
 go get -d gilab.com/logiq.tech/foxed.nesthorn
 cd $GOPATH/src/gilab.com/logiq.tech/foxed.nesthorn
 dep ensure
-
 ```
 
+## Run app
+
+```
+docker run -p9088:9088 -p6534:6534 -it reindexer/reindexer
+go run cmd/service
+```
 ## Running the tests
 
 ```
+docker run -p9088:9088 -p6534:6534 -it reindexer/reindexer
 cd tests
 ginkgo
 ```
-
-## PS
-
-В докер контейнер не стал класть, времени не хватило.
--- workers/foxer.go
+++ workers/foxer.go
@@ -5,7 +5,6 @@ import (
 	"github.com/buaazp/fasthttprouter"
 	"github.com/logiqone/foxed.nesthorn/model"
 	"github.com/restream/reindexer"
-	_ "github.com/restream/reindexer/bindings/builtin"
 	"github.com/valyala/fasthttp"
 	"strconv"
 	"time"
@@ -21,7 +20,8 @@ func (f *Foxer) Init() error {
 	f.StrContentType = []byte("Content-Type")
 	f.StrApplicationJSON = []byte("application/json")
 
-	f.Db = reindexer.NewReindex("builtin:///usersdb")
+	// f.Db = reindexer.NewReindex("builtin:///usersdb")
+	f.Db = reindexer.NewReindex("cproto://127.0.0.1:6534/usersdb")
 	return f.Db.OpenNamespace("users", reindexer.DefaultNamespaceOptions(), model.User{})
 }
 
@@ -36,17 +36,17 @@ func (f *Foxer) InitDataFill() (err error) {
 	fmt.Println("Start fill database...")
 
 	// Generate dataset
-	for i := 0; i < 5000000; i++ {
-		err = f.Db.Upsert("users", &model.User{
-			ID:          int64(i),
-			UserId: 	 int64(i),
-			Address:     "127.0.0." + strconv.Itoa(i),
-			Timestamp: 	time.Now().Format(time.RFC850),
-		})
-		if err != nil {
-			return err
-		}
-	}
+	//for i := 0; i < 1000000; i++ {
+	//	err = f.Db.Upsert("users", &model.User{
+	//		ID:          int64(i),
+	//		UserId: 	 int64(i),
+	//		Address:     "127.0.0." + strconv.Itoa(i),
+	//		Timestamp: 	time.Now().Format(time.RFC850),
+	//	})
+	//	if err != nil {
+	//		return err
+	//	}
+	//}
 
 	err = f.Db.Upsert("users", &model.User{ ID: 1, UserId: 1, Address: "127.0.0.1", Timestamp: time.Now().Format(time.RFC850) })
 	err = f.Db.Upsert("users", &model.User{ ID: 2, UserId: 2, Address: "127.0.0.1", Timestamp: time.Now().Format(time.RFC850) })
@@ -56,6 +56,11 @@ func (f *Foxer) InitDataFill() (err error) {
 	err = f.Db.Upsert("users", &model.User{ ID: 6, UserId: 3, Address: "127.0.0.3", Timestamp: time.Now().Format(time.RFC850) })
 	err = f.Db.Upsert("users", &model.User{ ID: 7, UserId: 3, Address: "127.0.0.1", Timestamp: time.Now().Format(time.RFC850) })
 	err = f.Db.Upsert("users", &model.User{ ID: 8, UserId: 4, Address: "127.0.0.1", Timestamp: time.Now().Format(time.RFC850) })
+
+	err = f.Db.Upsert("users", &model.User{ ID: 9, UserId: 3, Address: "127.0.0.7", Timestamp: time.Now().Format(time.RFC850) })
+	err = f.Db.Upsert("users", &model.User{ ID: 10, UserId: 3, Address: "127.0.0.7", Timestamp: time.Now().Format(time.RFC850) })
+	err = f.Db.Upsert("users", &model.User{ ID: 11, UserId: 3, Address: "127.0.0.7", Timestamp: time.Now().Format(time.RFC850) })
+	err = f.Db.Upsert("users", &model.User{ ID: 12, UserId: 3, Address: "127.0.0.7", Timestamp: time.Now().Format(time.RFC850) })
 
 	if err != nil {
 		return err
@@ -70,12 +75,12 @@ func (f *Foxer) Handler(ctx *fasthttp.RequestCtx) {
 	start := time.Now()
 	ctx.Response.Header.SetCanonical(f.StrContentType, f.StrApplicationJSON)
 
-	u1, err := strconv.Atoi(ctx.UserValue("user_id_1").(string))
+	u1, err := strconv.ParseInt(ctx.UserValue("user_id_1").(string), 10, 64)
 	if err != nil {
 		ctx.Response.SetStatusCode(fasthttp.StatusBadRequest)
 		return
 	}
-	u2, err := strconv.Atoi(ctx.UserValue("user_id_2").(string))
+	u2, err := strconv.ParseInt(ctx.UserValue("user_id_2").(string), 10, 64)
 	if err != nil {
 		ctx.Response.SetStatusCode(fasthttp.StatusBadRequest)
 		return
@@ -98,24 +103,40 @@ func (f *Foxer) Handler(ctx *fasthttp.RequestCtx) {
 	fmt.Printf("%s\n", time.Since(start).String())
 }
 
-func (f *Foxer) Compare(u1 int, u2 int) bool {
+func (f *Foxer) Compare(u1 int64, u2 int64) bool {
 	query := f.Db.Query("users").
-		WhereInt("user_id", reindexer.SET, u1, u2).
+		WhereInt64("user_id", reindexer.SET, u1, u2).
 		ReqTotal()
 
 	iterator := query.Exec()
 	defer iterator.Close()
 
-	duplicateFrequency := make(map[string]int)
+	u1UniqueIp := make(map[string]struct{})
+	u2UniqueIp := make(map[string]struct{})
 
-	iter := iterator.Count()
 	for iterator.Next() {
 		elem := iterator.Object().(*model.User)
-		duplicateFrequency[elem.Address]++
+		if elem.UserId == u1 {
+			u1UniqueIp[elem.Address] = struct{}{}
+		} else {
+			u2UniqueIp[elem.Address] = struct{}{}
+		}
 	}
 
+	frq := 0
+	for key := range u1UniqueIp {
+		_, ok := u2UniqueIp[key]
+		if ok {
+			frq++
+		}
+	}
+
+	fmt.Printf("u1: %v\n", u1UniqueIp)
+	fmt.Printf("u2: %v\n", u2UniqueIp)
+	fmt.Printf("frq: %v\n", frq)
+
 	var res bool
-	if iter - len(duplicateFrequency) >= 2 {
+	if frq >= 2 {
 		res = true
 	} else {
 		res = false
